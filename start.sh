10b997bd43e156ca01019c8bce41989af8d701bc
fakeu123445 <104671232+fakeu123445@users.noreply.github.com>
2023-03-02 05:37:21 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=61e3ee1a-0cc2-4a6b-b6d1-76724ed23aee
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=gamingerox.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
